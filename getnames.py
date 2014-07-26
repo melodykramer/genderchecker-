71e05e2c318c6da7d8d7cdd9db0a36de37657c79
scarothers <saracarothers@gmail.com>
2014-07-26 18:04:05 -0400

    Add the python-calais wrapper

--- getnames.py
+++ getnames.py
@@ -1,2 +1,13 @@
 # In this script, I'm going to use the OpenCalais API to send 
 # a chunk of text, and get back named entities in return. 
+
+import requests
+from calais import Calais
+
+
+API_KEY = "c7pg9t7qj6wtrs3a7fjnm99s"
+calais = Calais(API_KEY, submitter="s_carothers")
+
+result = calais.analyze_url("http://www.bestofsicily.com/mafia.htm")
+
+result.print_summary()
